6ed9ba8e7447268098652e9eeae87a21e2ea982d
Prw4nnabe <pw4nnabe@gmail.com>
2025-02-23 22:46:50 +0100

    blank page added for frontpage

---
+++ wp-content/themes/astra/templates/blankside.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Template name: Blank
+ */
+?>
+<?php get_header(); ?>
+
+<main class="front-page">
+    <section class="hero-section">
+        <div class="logo">
+            <img class="logo" src="https://blanjac.pl/wp-content/uploads/2025/01/blanjaclogo_inverted.png">
+        </div>
+        <div class="brand">
+            <h1 data-text="Blanjac">
+                Blanjac
+            </h1>
+        </div>
+        <div class="slogan">
+            <p>
+                Twój brakujący element w firmie
+            </p>
+        </div>
+    </section>
+    <section class="about-section">
+        <h2>Kim jesteśmy?</h2>
+        <div class="description">
+            <p>Każdy sukces to układanka, a my jesteśmy tym elementem, którego szukasz, by ją dopełnić. W Blanjac wierzymy, że każda firma ma inne potrzeby i wyzwania, dlatego dobieramy strategie indywidualnie, dopasowując je do Twoich celów i specyfiki branży. Nasze podejście oparte na growth marketingu to połączenie analityki, kreatywności i niestandardowych rozwiązań, które wykraczają poza schematy tradycyjnego marketingu internetowego. Pomagamy firmom rosnąć szybciej, efektywniej i mądrzej.</p>
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
